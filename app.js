2ae8973852c71b8227bb5a38b64f3c4cdd5c0d8e
hasansheikh1 <hasanfaysal17@gmail.com>
2022-09-29 20:31:09 +0500

    second comm

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 let myBtn = document.getElementById("myBtn");
-
+let print=document.getElementById("content");
 let content=document.getElementById("content");
 
 function getData(){
@@ -11,10 +11,18 @@ function getData(){
         return response.text();
     }).then((data)=>{
         console.log("inside second then");
+
+        print.innerHTML=data;
         console.log(data);
     })
 }
 
 console.log("Before running");
-getData();
+// getData();
 console.log("After running getData");
+
+// function postData(){s
+
+//     url = "https://dummy.restapiexample.com/api/v1/create";
+//     data = '{"name":"test","salary":"123","age":"23"}'
+// }
